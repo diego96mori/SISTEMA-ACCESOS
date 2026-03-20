ef2c6fe81ec2c3358e87d80f0ad8f9c26ee640d2
diego96mori <diego96mori@gmail.com>
2026-03-20 17:17:36 -0500

    modificacion de trabajo contrata25

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://stkgsygonyxtrdhlgusx.supabase.co'
-const supabaseAnonKey = 'sb_publishable_lzLwfUm87890uaQlGaByJw_iuW0PCq4'
+export const supabaseUrl = 'https://stkgsygonyxtrdhlgusx.supabase.co'
+export const supabaseAnonKey = 'sb_publishable_lzLwfUm87890uaQlGaByJw_iuW0PCq4'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
