d95ea6e92a883f0f2b244ccecb7af587dd73f3c2
diego96mori <diego96mori@gmail.com>
2026-03-20 19:52:11 -0500

    modificacion de trabajo contrata38

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -266,7 +266,7 @@ console.log("KEY:", supabaseAnonKey);
                   <th className="p-4 min-w-[140px]">Nodo</th>
                   <th className="p-4 min-w-[120px]">Fecha</th>
                   <th className="p-4 min-w-[100px]">Hora</th>
-                  <th className="p-4 min-w-[140px]">Solicitante</th>
+                  <th className="p-4 min-w-[160px]">Solicitante</th>
                   <th className="p-4">Documento</th>
                   <th className="p-4">Teléfono</th>
                   <th className="p-4 min-w-[140px]">Empresa</th>
@@ -304,8 +304,8 @@ console.log("KEY:", supabaseAnonKey);
                     <td className="p-4">{a.fecha_ingreso}</td>
                     <td className="p-4">{a.hora_ingreso}</td>
 
-                    <td className="p-4">
-                      {a.solicitante_nombre} {a.solicitante_ap_paterno} {a.solicitante_ap_materno}
+                    <td className="p-4 max-w-[160px] truncate">
+                    {a.solicitante_nombre} {a.solicitante_ap_paterno} {a.solicitante_ap_materno}
                     </td>
 
                     <td className="p-4">{a.solicitante_num_doc}</td>
