dfd5fcf1da1ca36c51226432d05907199bbe28d9
diego96mori <diego96mori@gmail.com>
2026-03-26 17:50:06 -0500

    modificacion de trabajo contrata73

--- src/Pages/RacksView.jsx
+++ src/Pages/RacksView.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { supabase } from "../supabaseClient";
+import { useNavigate } from "react-router-dom";
 
 function RacksView() {
 
@@ -9,6 +10,7 @@ function RacksView() {
 
   const [nodoId, setNodoId] = useState("");
   const [rackId, setRackId] = useState("");
+  const navigate = useNavigate();
 
   useEffect(() => {
     cargarNodos();
@@ -71,10 +73,41 @@ function RacksView() {
     setRuData(resultado);
   };
 return (
+
+ 
+  
   <div className="min-h-screen bg-gray-100 flex">
 
+    {/* SIDEBAR */}
+<aside className="w-64 bg-white shadow-lg p-6">
+
+  <h2 className="text-xl font-bold mb-6">WI-NET</h2>
+
+  <button
+    onClick={() => navigate("/dashboard")}
+    className="w-full text-left px-4 py-2 rounded-lg hover:bg-gray-200"
+  >
+    Lista de Accesos
+  </button>
+
+  <button
+    onClick={() => navigate("/equipos")}
+    className="w-full text-left px-4 py-2 rounded-lg mt-2"
+  >
+    Gestión Equipos
+  </button>
+
+  <button
+    className="w-full text-left px-4 py-2 rounded-lg bg-blue-600 text-white mt-2"
+  >
+    Vista de Racks
+  </button>
+
+</aside>
+
     {/* CONTENEDOR PRINCIPAL */}
-    <div className="bg-white rounded-2xl shadow-lg p-6 w-[700px] mx-auto mt-10">
+    <div className="flex-1 p-8">
+  <div className="bg-white rounded-2xl shadow-lg p-6 max-w-[700px]"></div>
 
       {/* 🔹 TÍTULO */}
       <h2 className="text-2xl font-bold text-center mb-6 text-gray-800">
