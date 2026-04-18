3e9a303d6e82231610112272ce1fe2236675a380
diego96mori <diego96mori@gmail.com>
2026-04-17 19:07:45 -0500

    netbox_2

--- src/Pages/Instalaciones.jsx
+++ src/Pages/Instalaciones.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { supabase } from "../supabaseClient";
-import { netboxGet } from "../api/netbox";
+import { netboxGet } from "../Netbox";
 import "./Instalaciones.css";
 
 function Instalaciones() {
